1595a1251b954578c649f783d5bd6ceef69dd0d9
Philipp Stappert <mail@phsta.de>
2024-11-06 15:02:42 +0000

    make it possible to do defaultOrderBy with orderByRaw

--- src/Traits/DefaultOrderBy.php
+++ src/Traits/DefaultOrderBy.php
@@ -8,18 +8,23 @@ trait DefaultOrderBy
 {
     protected static function bootDefaultOrderBy()
     {
-        if (empty(self::$orderByColumn)) {
+        if (empty(self::$orderByColumn) && empty(self::$orderByRawColumn)) {
             return;
         }
 
         $column = self::$orderByColumn;
+        $rawColumn = self::$orderByRawColumn;
 
         $direction = isset(self::$orderByColumnDirection)
             ? self::$orderByColumnDirection
             : config('default-model-sorting.order_by');
 
-        static::addGlobalScope('default_order_by', function (Builder $builder) use ($column, $direction) {
-            $builder->orderBy($column, $direction);
+        static::addGlobalScope('default_order_by', function (Builder $builder) use ($column, $rawColumn, $direction) {
+            if (!empty($rawColumn)) {
+                $builder->orderByRaw($rawColumn);
+            } else {
+                $builder->orderBy($column, $direction);
+            }
         });
     }
 }
